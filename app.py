6fcc7d2e57ff731cde9c1935da695c0b9bd3efac
Umakantha Shankar <skmadhumaddy@gmail.com>
2021-01-06 15:49:59 +0530

    Adding configuration for the DB PostGres from Heroku

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from security import authenticate, identity
@@ -8,7 +10,8 @@ from flask_jwt import JWT
 from resources.user import userregister
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 app.secret_key = 'MADHU'
 api = Api(app)
